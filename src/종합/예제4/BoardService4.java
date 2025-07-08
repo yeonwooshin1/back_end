0fb85a37aa6134545149ab6c2ab93fa221387efa
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-08 16:02:24 +0900

    나만의 주석

--- src/종합/예제4/BoardService4.java
+++ src/종합/예제4/BoardService4.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class BoardService4 {    // class start
     public static void main(String[] args) {    // main start
 
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);  // Scanner 하는 거 부여한다 어디다가? scan이라는 변수에다가
 
-        BoardController bs = new BoardController();
+        BoardController bs = new BoardController();     // BoardController 클래스를 갖다써줄 객체( 클래스 분신 )를 부여한다 어디다가? bs라는 변수에다가
 
         for( ; ; ){
             System.out.println("============= My Community =============");
@@ -18,17 +18,17 @@ public class BoardService4 {    // class start
             if( choose == 1 ){
                 System.out.print("작성자 : ");   String writerInput  = scan.next();
                 System.out.print("내용 : ");    String contentInput = scan.next();
-                boolean alert = bs.doPost( contentInput, writerInput );
-                if(alert){System.out.println("[안내] 등록 성공"); }
-                else{ System.out.println("[경고] 등록 실패");}
+                boolean alert = bs.doPost( contentInput, writerInput ); // boolean 타입인 메소드인 doPost를 데려오는데 매개변수는 scan받은 Writer와 content를 넣어줌 그것을 alert라는 변수에 할당
+                if(alert){System.out.println("[안내] 등록 성공"); }   // alert가 true라면 등록 성공
+                else{ System.out.println("[경고] 등록 실패");}    // alert가 false라면 등록 실패
             }   // if choose == 1 end
             if(choose == 2){
-                Board[] boardArray = bs.doGet();
-                for(Board board : boardArray){
+                Board[] boardArray = bs.doGet();        // 반환값이 Board[] 배열인 doGet을 가져오는데 변수는 boardArray에다가
+                for(Board board : boardArray){          // boardArray == 배열을 모두 반복하는 반복문 돌리는데 그것의 인자들은 board
                     if( board != null ){
                         System.out.println("============= 게시물 목록 =============");
-                        System.out.println("작성자 : " + board.writer);
-                        System.out.println("내용 : " + board.content);
+                        System.out.println("작성자 : " + board.writer);    // board 객체에 있는 멤버변수 writer를 가져온다.
+                        System.out.println("내용 : " + board.content);     // board 객체에 있는 멤버변수 content를 가져온다.
                         System.out.println("------------------------------------");
                     }   // if end
                 }   // for i end
