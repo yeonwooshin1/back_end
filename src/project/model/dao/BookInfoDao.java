5b4127303ced04df600c878cb2c20ceeb9af22eb
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-15 16:48:17 +0900

    팀플 커밋푸시 dto

---
+++ src/project/model/dao/BookInfoDao.java
@@ -0,0 +1,4 @@
+package project.model.dao;
+
+public class BookInfoDao {
+}
---
+++ src/project/model/dto/BookListDto.java
@@ -0,0 +1,48 @@
+package project.model.dto;  // package
+
+public class BookListDto {  // class BookListDto start
+    // 멤버변수
+    private int bookLog;    // 도서 목록 코드
+    private int bookNo;     // 도서 정보 코드 ( BookInfoDto )
+
+    // 생성자
+    public BookListDto(){}
+
+    // 책 대출 생성자
+
+    public BookListDto(int bookLog, int bookNo) {
+        this.bookLog = bookLog;
+        this.bookNo = bookNo;
+    }   // func 책 대출 생성자 end
+
+
+    // getter setter
+
+
+    public int getBookLog() {
+        return bookLog;
+    }
+
+    public void setBookLog(int bookLog) {
+        this.bookLog = bookLog;
+    }
+
+    public int getBookNo() {
+        return bookNo;
+    }
+
+    public void setBookNo(int bookNo) {
+        this.bookNo = bookNo;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "BookListDto{" +
+                "bookLog=" + bookLog +
+                ", bookNo=" + bookNo +
+                '}';
+    }
+
+}   // class BookListDto end
