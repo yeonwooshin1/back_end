32f988b38dbd023fbbf72d880725ace43e2226b3
yeonwooshin1 <tlswnsgur456@gmail.com>
2025-07-23 22:30:36 +0900

    예제10 end

--- src/종합/예제10/model/dao/BoardDao.java
+++ src/종합/예제10/model/dao/BoardDao.java
@@ -1,25 +1,108 @@
 package 종합.예제10.model.dao;  // package
 
+import 종합.예제10.model.dto.BoardDto;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 public class BoardDao { // class start
     // 싱글톤 만들기
-    private BoardDao(){}
+    private BoardDao(){
+        connectDB();
+    }
     private static final BoardDao dao = new BoardDao();
     public static BoardDao getInstance() {
         return dao;
     }
+
     // DB 정보입력
+    private String db_url = "jdbc:mysql://localhost:3306/boardService";
+    private String db_user = "root";
+    private String db_password = "1234";
 
     // Connection 연결 메소드
+    private Connection getConnection () throws SQLException {
+        return DriverManager.getConnection( db_url , db_user , db_password );
+    }   // func end
 
     // DB 연동 메소드 Class.forName("com.mysql.cj.jdbc.Driver");
+    private void connectDB () {
+        try{
+            // mysql 를 지정한 Driver 클래스 가져오기
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch ( ClassNotFoundException e ) { System.out.println("[실패] mysql 드라이버 연동 실패"); } // 클래스 있는지 잘 연결됐는지 예외
+    }   // func end
+
+
 
     // Board 등록 메소드
+    public boolean BoardWrite( BoardDto boardDto ) {
+       String sql = "insert into board( bwriter , bcontent ) values (?,?)";
+       try {
+           Connection conn = getConnection();
+           PreparedStatement ps = conn.prepareStatement( sql );
+           ps.setString(1, boardDto.getBwriter() );
+           ps.setString(2, boardDto.getBcontent() );
+           int result = ps.executeUpdate();
+
+           return result == 1;
+       } catch ( SQLException e ) { // 데이터 베이스 접근 예외
+           System.out.println("[경고] 데이터베이스 접근 실패");
+           return false;
+       }    // catch end
+    }   // func end
 
     // Board 출력 메소드
+    public ArrayList<BoardDto> BoardPrint() {
+        String sql = "select bno , bwriter , bcontent from board";
+        ArrayList<BoardDto> list = new ArrayList<>();
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement( sql );
+            ResultSet rs = ps.executeQuery();
+            while( rs.next() ) {
+                BoardDto dto = new BoardDto();
+                dto.setBno(rs.getInt("bno"));
+                dto.setBwriter(rs.getString("bwriter"));
+                dto.setBcontent(rs.getString("bcontent"));
+                list.add(dto);
+            }   // while end
+        } catch (SQLException e) {
+            System.out.println("[경고] 데이터베이스에서 가져오기 실패");
+        }    // catch end
+        return list;
+    }   // func end
 
     // Board 삭제 메소드
+    public boolean BoardDelete (int bno ) {
+        String sql = "delete from board where bno = ?";
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement( sql );
+            ps.setInt(1, bno);
+            int result = ps.executeUpdate();
+            return result == 1;
+        } catch ( SQLException e ) {
+            System.out.println("[경고] 데이터베이스 오류");
+            return false;
+        }    // catch end
+    }   // func end
 
     // Board 수정 메소드
-
+    public boolean BoardUpdate ( BoardDto boardDto ) {
+        String sql = "update board set bcontent = ? where bno = ?";
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareStatement( sql );
+            ps.setString(1 , boardDto.getBcontent() );
+            ps.setInt(2, boardDto.getBno() );
+            int result = ps.executeUpdate();
+            return result == 1;
+        } catch ( SQLException e ) {
+            System.out.println("[경고] 데이터베이스 오류");
+            return false;
+        }    // catch end
+    }   // func end
 
 }   // class end
--- src/종합/예제10/view/BoardView.java
+++ src/종합/예제10/view/BoardView.java
@@ -24,9 +24,9 @@ public class BoardView {    // class start
     // index 시작 view
     public void index () {
         for ( ; ; ) {
-            System.out.println("============= My Community =============");
-            System.out.println("  1.등록 | 2.전체조회 ");
-            System.out.println("========================================");
+            System.out.println("=================== My Community ===================");
+            System.out.println("1.게시물쓰기 | 2.게시물출력 | 3.게시물삭제 | 4.게시물수정 ");
+            System.out.println("====================================================");
             System.out.print("선택 > ");
             try {
                 int choose = scan.nextInt();
@@ -45,16 +45,25 @@ public class BoardView {    // class start
     // Board 등록 view
     public void BoardWrite() {
         // Scanner 입력받기
-        System.out.print("내용 : ");      String bcontentInput = scan.next();
+        scan.nextLine();
+        System.out.print("내용 : ");      String bcontentInput = scan.nextLine();
         System.out.print("작성자 : ");     String bwriterInput = scan.next();
 
         boolean result = controller.BoardWrite( bcontentInput , bwriterInput );
+        if(result) System.out.println("[성공] 글쓰기 성공");
+        else System.out.println("[실패] 글쓰기 실패");
     }   // func end
 
     // Board 출력 view
     public void BoardPrint() {
-
+        System.out.println("============= 게시물 목록 =============");
         ArrayList<BoardDto> BoardDB = controller.BoardPrint();
+        for (BoardDto index : BoardDB) {
+            System.out.println("번호 : " + index.getBno());
+            System.out.println("작성자 : " + index.getBwriter());
+            System.out.println("내용 : " + index.getBcontent());
+            System.out.println("------------------------------------");
+        }   // for end
     }   // func end
 
     // Board 삭제 view
@@ -63,27 +72,40 @@ public class BoardView {    // class start
         int bnoInput;
         for ( ; ; ){
             try {
-                System.out.println("삭제할번호 : ");     bnoInput = scan.nextInt();
-
+                System.out.print("삭제할번호 : ");     bnoInput = scan.nextInt();
                 if (bnoInput > 0) break;
-                else System.out.println("[경고] 정상적인 숫자를 입력하세요.");
+                else System.out.println("[경고] 양수만 입력하세요.");
             } catch ( InputMismatchException e) {
                 System.out.println("[경고] 숫자만 입력하세요.");
                 scan.nextLine();
             }   // catch end
-        }
-
-
-
+        }   // for end
 
         boolean result = controller.BoardDelete( bnoInput );
+        if (result) System.out.println("[안내] 삭제 성공");
+        else System.out.println("[경고] 삭제 실패");
     }   // func end
 
     // Board 수정 view
     public void BoardUpdate() {
+        System.out.println("============= 게시물 목록 =============");
+        int bnoInput;           String bcontentInput;
+        for( ;; ) {
+            try {
+                System.out.print("수정할 번호 : ");  bnoInput = scan.nextInt();
+                scan.nextLine();
+                System.out.print("새로운 내용 : "); bcontentInput = scan.nextLine();
 
+                if (bnoInput > 0) break;
+                else System.out.println("[경고] 양수만 입력하세요.");
+            } catch (InputMismatchException e) {
+                System.out.println("[경고] 숫자만 입력하세요.");
+            }   // catch end
+        }   // for end
 
         boolean result = controller.BoardUpdate( bnoInput , bcontentInput );
+        if (result) System.out.println("[안내] 수정 성공");
+        else System.out.println("[경고] 수정 실패");
     }   // func end
 
 }   // class end
